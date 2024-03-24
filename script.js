76006c708564d7fdfe3bed253b2143cc2fd2d332
rlop74 <119091421+rlop74@users.noreply.github.com>
2024-03-23 23:46:00 -0400

    cleaned code and added comments

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function addToDo() {
     const getItem = document.getElementById("new-todo");
     const newItem = getItem.value;
-    getItem.value = "";
-
+    const list = document.getElementById("list");
+    const li = document.createElement("li");
+    const addList = document.createTextNode(newItem);
+    
+    // prompt error if input field is empty
     if (newItem.length <= 0) {
         alert("bat walang laman");
         skip;
     }
 
-    const list = document.getElementById("list");
-    const li = document.createElement("li");
-    const addList = document.createTextNode(newItem);
+    // clear input field
+    getItem.value = "";
     
+    // add text content to li tag
+    li.appendChild(addList);
+    list.appendChild(li);
+
+    // remove li when clicked
     li.setAttribute("onclick", "this.remove()");
 
-    li.appendChild(addList)
-    list.appendChild(li);
-    
 }
